0b34ce1bf75845124243d133f6047c6affebb3ef
Nicole Park <nicolepark@bellsouth.net>
2023-11-13 15:45:36 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
+import AllPuppies from "./components/AllPuppies";
+import AddPuppyForm from "./components/AddPuppyForm";
+import SinglePuppy from "./components/SinglePuppy";
+import "./App.css";
 
-import './App.css'
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  
+    return (
+        <>
+            <h1>Puppy Bowl</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/add">Add Puppy</Link>
+            </nav>
 
-  return (
-    <>
-     
-    </>
-  )
+            <Routes>
+                <Route path="/" element={<AllPuppies />} />
+                <Route path="/add" element={<AddPuppyForm />}></Route>
+                <Route path="/details/:id" element={<SinglePuppy />}></Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
